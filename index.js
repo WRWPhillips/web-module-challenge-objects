d5cfb4a103cc70cdec2b4f1bc3606283abdac3dc
WRWPhillips <42WPhillips42@gmail.com>
2021-11-04 13:00:47 -0500

    finished first problem

--- index.js
+++ index.js
@@ -15,8 +15,13 @@ The function should:
   Example createMenuItem('tacos', 8, 'Lunch') should return {name: 'tacos', price: 8, category: 'Lunch'}
 */
 
-function createMenuItem(/*Your code here*/){
-    /*Your code here*/
+function createMenuItem(name, price, category){
+    const menuItem = {
+      name,
+      price,
+      category
+    }
+  return menuItem;
 }
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1b: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -29,7 +34,13 @@ Test your createMenuItems function by doing the following:
   For example: createMenuItem("pizza",5,"lunch") would return this as the object: {name:"Pizza",price:5,category:"lunch"}
 */
 
+const pizzaItem = createMenuItem('pizza', 7, 'lunch');
+const nachoItem = createMenuItem('nachos', 14, 'lunch');
+const potatoItem = createMenuItem('potato', 5, 'dinner');
 
+console.log(pizzaItem);
+console.log(nachoItem);
+console.log(potatoItem);
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 You're having a lunch special! 25% off for teachers and students, 10% off for everyone else. Add a method to the 
